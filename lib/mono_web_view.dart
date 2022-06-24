fdc3738809e309129315da5b37691c4f8bebe669
Tim Oladoyinbo <66416657+timoladoyinbo@users.noreply.github.com>
2022-06-24 03:36:24 +0100

    Update mono_web_view.dart

--- lib/mono_web_view.dart
+++ lib/mono_web_view.dart
@@ -84,7 +84,7 @@ class MonoWebViewState extends State<MonoWebView> {
       child: Material(
         child: GestureDetector(
             onTap: () {
-              WidgetsBinding.instance.focusManager.primaryFocus?.unfocus();
+              WidgetsBinding.instance?.focusManager.primaryFocus?.unfocus();
             },
             child: SafeArea(
               child: Stack(children: [
