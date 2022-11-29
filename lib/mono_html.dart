7b12880cf4dfd87b606e9c92dd40538542d7d736
Benjamin Akhigbe <51957344+CodeAvid@users.noreply.github.com>
2022-11-29 12:00:04 +0100

    feat: :white_check_mark: Add mono payment view
    This enable flutter sdk to use mono direct pay

--- lib/mono_html.dart
+++ lib/mono_html.dart
@@ -51,4 +51,70 @@ class MonoHtml {
       </script>
     </body>
 </html>''';
+
+  /// Raw mono html formation
+  static String buildPaymentView(
+    String? key,
+    String? paymentId, [
+    Map<String, dynamic>? config,
+    String reference = '',
+    String? authCode,
+  ]) =>
+      '''
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mono Connect</title>
+</head>
+
+<body onload="setupMonoConnect()" style="border-radius: 20px; background-color:#fff;height:100vh;overflow: hidden; ">
+    <script src="https://connect.withmono.com/connect.js"></script>
+    <script type="text/javascript">
+        window.onload = setupMonoConnect;
+        function setupMonoConnect() {
+            var connect = new Connect({
+                key: "$key",
+                scope: "payments",
+                reference: `$reference`.length > 0 ? `$reference` : null,
+                data: {
+                    payment_id: "$paymentId",
+                },
+                onSuccess: (code) =>
+                    sendMessage({ type: "onSuccess", data: code}),
+                onLoad: () => sendMessage({ type: "onLoad" }),
+                onClose: () => sendMessage({ type: "onClose" }),
+                onEvent: (eventName, data) =>
+                  sendMessage({
+                    type: "onEvent",
+                    eventName: eventName,
+                    data: { ...data },
+                  }),
+            });
+            
+
+          const configJson = JSON.parse(`${jsonEncode(config ?? {})}`)
+          const authCode = "$authCode";
+          
+          connect.setup(configJson);
+
+          if(authCode && String(authCode).length > 2) { 
+            connect.reauthorise(authCode);
+          }
+            
+            connect.open()
+            function sendMessage(message) {
+              if (window.MonoClientInterface && window.MonoClientInterface.postMessage) {
+                  MonoClientInterface.postMessage(JSON.stringify(message));
+              }
+          } 
+        }
+    </script>
+</body>
+
+</html>
+''';
 }
