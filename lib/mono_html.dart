83cd4882d67b4b744141f75f9b8f3b0ba65b44df
Benjamin Akhigbe <51957344+CodeAvid@users.noreply.github.com>
2022-11-29 13:25:27 +0100

    fix(update): :ambulance: mono payment response

--- lib/mono_html.dart
+++ lib/mono_html.dart
@@ -52,69 +52,63 @@ class MonoHtml {
     </body>
 </html>''';
 
-  /// Raw mono html formation
-  static String buildPaymentView(
+  static buildPaymentView(
     String? key,
     String? paymentId, [
     Map<String, dynamic>? config,
     String reference = '',
     String? authCode,
   ]) =>
-      '''
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mono Connect</title>
-</head>
-
-<body onload="setupMonoConnect()" style="border-radius: 20px; background-color:#fff;height:100vh;overflow: hidden; ">
-    <script src="https://connect.withmono.com/connect.js"></script>
-    <script type="text/javascript">
+      '''<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Mono Connect</title>
+    </head>
+    <body onload="setupMonoConnect()" style="background-color:#fff;height:100vh;overflow: scroll;">
+      <script src="https://connect.withmono.com/connect.js"></script>
+      <script type="text/javascript">
         window.onload = setupMonoConnect;
         function setupMonoConnect() {
-            var connect = new Connect({
-                key: "$key",
-                scope: "payments",
-                reference: `$reference`.length > 0 ? `$reference` : null,
-                data: {
-                    payment_id: "$paymentId",
-                },
-                onSuccess: (code) =>
-                    sendMessage({ type: "onSuccess", data: code}),
-                onLoad: () => sendMessage({ type: "onLoad" }),
-                onClose: () => sendMessage({ type: "onClose" }),
-                onEvent: (eventName, data) =>
-                  sendMessage({
-                    type: "onEvent",
-                    eventName: eventName,
-                    data: { ...data },
-                  }),
-            });
-            
-
+          const options = {
+             key: "$key",
+             reference: "$reference",
+             scope: "payments",
+             data: {
+                  payment_id: "$paymentId",
+            },
+            onSuccess:  (data) => {
+              const response = {"type":"mono.modal.linked", response: {...data}}
+              MonoClientInterface.postMessage(JSON.stringify(response))
+            },
+             onEvent: (eventName, data) => {
+             const response = { type: 'onEvent', eventName, data }
+              MonoClientInterface.postMessage(JSON.stringify(response))
+            },
+            onClose: ()=> {
+              const response = {type: 'mono.modal.closed', }
+              MonoClientInterface.postMessage(JSON.stringify(response))
+            },
+              onLoad: ()=> {
+              const response = {type: 'mono.modal.onLoad', }
+              MonoClientInterface.postMessage(JSON.stringify(response))
+            }
+          };
+          const MonoConnect = new Connect(options);
           const configJson = JSON.parse(`${jsonEncode(config ?? {})}`)
           const authCode = "$authCode";
-          
-          connect.setup(configJson);
+
+          MonoConnect.setup(configJson);
 
           if(authCode && String(authCode).length > 2) { 
-            connect.reauthorise(authCode);
+            MonoConnect.reauthorise(authCode);
           }
-            
-            connect.open()
-            function sendMessage(message) {
-              if (window.MonoClientInterface && window.MonoClientInterface.postMessage) {
-                  MonoClientInterface.postMessage(JSON.stringify(message));
-              }
-          } 
-        }
-    </script>
-</body>
 
-</html>
-''';
+          MonoConnect.open()
+        }
+      </script>
+    </body>
+</html>''';
 }
