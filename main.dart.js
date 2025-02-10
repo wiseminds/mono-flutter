e0767627db6a7f9699d4cdfe102130d778904992
wiseminds <wiseminds@users.noreply.github.com>
2025-02-10 00:48:28 +0000

    deploy: 997f7592fff7bdeb6222283fcb799b11b3d4be11

--- main.dart.js
+++ main.dart.js
@@ -18345,7 +18345,7 @@ A.a1q.prototype={
 $1(a){var s=A.cK().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/e672b006cb34c921db85b8e2f482ed3144a4574b/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/82bd5b7209295a5b7ff8cae0df96e7870171e3a5/":s)+a},
 $S:75}
 A.Kx.prototype={
 df(a){B.c.S(this.a.a.save())},
